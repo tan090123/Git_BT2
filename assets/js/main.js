d7df38bfab5094dc5ab789678a31c9f42dbe5e37
tan090123 <mtan23102003@gmail.com>
2023-03-18 07:36:13 +0700

    Update animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,30 @@
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
+// const box1 = document.querySelector('.box1');
+// const box2 = document.querySelector('.box2');
+// const box3 = document.querySelector('.box3');
 
-box1.addEventListener('click', function () {
-    box2.style.display = "block";
-    box1.style.display = "none";
-    box3.style.display = "none";
-});
+// box1.addEventListener('click', function () {
+//     box2.style.display = "block";
+//     box1.style.display = "none";
+//     box3.style.display = "none";
+// });
 
-box2.addEventListener('click', function () {
-    box3.style.display = "block";
-    box1.style.display = "none";
-    box2.style.display = "none";
-});
+// box2.addEventListener('click', function () {
+//     box3.style.display = "block";
+//     box1.style.display = "none";
+//     box2.style.display = "none";
+// });
 
-box3.addEventListener('click', function () {
-    box3.style.display = "none";
-    box1.style.display = "block";
-    box2.style.display = "none";
-});
+// box3.addEventListener('click', function () {
+//     box3.style.display = "none";
+//     box1.style.display = "block";
+//     box2.style.display = "none";
+// });
+
+
+function handleColorChange() {
+    var boxList = document.querySelectorAll('.box');
+    var tmp = boxList[2].style.backgroundColor;
+    boxList[2].style.backgroundColor = boxList[0].style.backgroundColor;
+    boxList[0].style.backgroundColor = boxList[1].style.backgroundColor;
+    boxList[1].style.backgroundColor = tmp;
+}
